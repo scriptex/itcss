b68d0354e52531401a0a1c183ded4af29dd2f108
Atanas Atanasov <scriptex.bg@gmail.com>
2021-11-17 14:34:53 +0200

    Upgrade postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,20 @@
 module.exports = {
-	plugins: [
-		require('postcss-easy-import'),
-		require('postcss-preset-env')({
-			stage: 0,
-			features: {
-				'color-mod-function': {
-					importFrom: 'assets/settings/_colors.css'
-				}
-			}
-		}),
-		require('postcss-normalize')({
+	plugins: {
+		autoprefixer: {},
+		'postcss-mixins': {},
+		'postcss-easy-import': {},
+		'postcss-normalize': {
 			forceImport: true
-		}),
-		require('postcss-nested'),
-		require('postcss-each-variables'),
-		require('postcss-each'),
-		require('postcss-for'),
-		require('postcss-mixins'),
-		require('postcss-extend'),
-		require('postcss-utilities'),
-		require('postcss-flexbugs-fixes'),
-		require('postcss-calc'),
-		require('postcss-custom-media')
-	]
+		},
+		'postcss-nested': {},
+		'postcss-color-mod-function': {},
+		'postcss-each-variables': {},
+		'postcss-each': {},
+		'postcss-for': {},
+		'postcss-extend': {},
+		'postcss-utilities': {},
+		'postcss-flexbugs-fixes': {},
+		'postcss-calc': {},
+		'postcss-custom-media': {}
+	}
 };
